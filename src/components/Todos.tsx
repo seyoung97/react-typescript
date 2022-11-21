3858f6b96fbe83402f7680c6bc421d73fd7519a9
Joey <seyoung1st@gmail.com>
2022-11-21 20:24:00 +0900

    Add: todo 타입 지정

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Todo from "../models/todo";
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
-      {props.items.map((item) => (
-        <li key={item}>{item}</li>
+      {items.map((item) => (
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );

da555ea7787370712cf925c7b331d15676830dfd
Joey <seyoung1st@gmail.com>
2022-11-22 15:49:25 +0900

    Add: 컴포넌트 생성하고 타입지정

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const TodoItem: React.FC<{ text: string }> = ({ text }) => {
+  return <li>{text}</li>;
+};
+export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text}></TodoItem>
       ))}
     </ul>
   );
 };
+
 export default Todos;

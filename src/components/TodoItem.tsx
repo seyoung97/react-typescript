1e878f2823bc551b1539104b8c6f5c71d4856cfb
Joey <seyoung1st@gmail.com>
2022-11-23 13:03:55 +0900

    Add: remove 기능 추가 및 type관리

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = ({ text }) => {
-  return <li className={classes.item}>{text}</li>;
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = ({
+  text,
+  onRemoveTodo,
+}) => {
+  return (
+    <div>
+      <li className={classes.item} onClick={onRemoveTodo}>
+        {text}
+      </li>
+    </div>
+  );
 };
 export default TodoItem;

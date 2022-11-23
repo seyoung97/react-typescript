5b76646b674789342ec7e3397709f4ff6449fab5
Joey <seyoung1st@gmail.com>
2022-11-23 12:22:35 +0900

    Add: css 적용

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {items.map((item) => (
         <TodoItem key={item.id} text={item.text}></TodoItem>
       ))}
